f7391686f997ca4ea54aee0d0d2e9dceb42837be
douglas <douglas.arau@hotmail.com>
2025-04-22 16:24:32 -0300

    conclusao do placar do flappy bird

--- js/script.js
+++ js/script.js
@@ -281,3 +281,25 @@ function criaCanos() {
 
   return canos;
 }
+
+function criaPlacar() {
+  const placar = {
+    pontuacao: 0,
+    desenha() {
+      contexto.font = '20px "Press Start 2P"';
+      contexto.textAlign = 'right';
+      contexto.fillStyle = 'white';
+      contexto.border = "black";
+      contexto.fillText(`${placar.pontuacao}`, canvas.width - 10, 35);      
+    },
+    atualiza() {
+      const intervaloDeFrames = 20;
+      const passouOIntervalo = frames % intervaloDeFrames === 0;
+
+      if(passouOIntervalo) {
+        placar.pontuacao = placar.pontuacao + 1;
+      }
+    }
+  }
+  return placar;
+};
